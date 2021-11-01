c8f6a15a8bf2dd0671e29e0b4ed51f3bd04a5bae
Steffen Vogel <post@steffenvogel.de>
2021-11-02 00:42:42 +0100

    add more run tests
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- pkg/run_test.go
+++ pkg/run_test.go
@@ -1,10 +1,12 @@
 package gont_test
 
 import (
+	"fmt"
 	"io/ioutil"
 	"testing"
 
 	g "github.com/stv0g/gont/pkg"
+	"github.com/vishvananda/netns"
 )
 
 func prepare(t *testing.T) (*g.Network, *g.BaseNode) {
@@ -38,6 +40,68 @@ func TestRun(t *testing.T) {
 	}
 }
 
+func TestRunFunc(t *testing.T) {
+	n, n1 := prepare(t)
+	defer n.Close()
+
+	// Run
+	if err := n1.RunFunc(func() error {
+		handle, err := netns.Get()
+		if err != nil {
+			return err
+		}
+
+		if !handle.Equal(n1.NsHandle) {
+			t.Errorf("mismatching netns handles")
+			t.FailNow()
+		}
+
+		return nil
+	}); err != nil {
+		t.Errorf("Failed to run identify: %s", err)
+	}
+}
+
+func TestRunGo(t *testing.T) {
+	n, n1 := prepare(t)
+	defer n.Close()
+
+	out, cmd, err := n1.RunGo("../cmd/gontc/gontc.go", "identify")
+	if err != nil {
+		t.Errorf("Failed to run Go script: %s", err)
+		t.FailNow()
+	}
+
+	if !cmd.ProcessState.Exited() || !cmd.ProcessState.Success() {
+		t.FailNow()
+	}
+
+	if string(out) != fmt.Sprintf("%s\n", n1) {
+		t.FailNow()
+	}
+}
+
+func TestEnter(t *testing.T) {
+	n, n1 := prepare(t)
+	defer n.Close()
+
+	exit, err := n1.Enter()
+	if err != nil {
+		t.FailNow()
+	}
+	defer exit()
+
+	handle, err := netns.Get()
+	if err != nil {
+		t.FailNow()
+	}
+
+	if !handle.Equal(n1.NsHandle) {
+		t.Errorf("mismatching netns handles")
+		t.FailNow()
+	}
+}
+
 func TestRunSimple(t *testing.T) {
 	n, n1 := prepare(t)
 	defer n.Close()
@@ -66,6 +130,10 @@ func TestStart(t *testing.T) {
 	}
 
 	cmd.Wait()
+
+	if !cmd.ProcessState.Exited() || !cmd.ProcessState.Success() {
+		t.FailNow()
+	}
 
 	if string(out) != n1.Namespace.Name+"\n" {
 		t.Errorf("Got invalid namespace: %s", string(out))
