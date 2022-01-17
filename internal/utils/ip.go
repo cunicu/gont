cb6143d68d2bcd88119737a6a3efb81ce6644f92
Steffen Vogel <post@steffenvogel.de>
2022-01-17 10:48:55 +0100

    convert net.IPNet CIDR to address range
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

---
+++ internal/utils/ip.go
@@ -0,0 +1,55 @@
+package utils
+
+import (
+	"fmt"
+	"math/big"
+	"net"
+)
+
+func ipToInt(ip net.IP) (*big.Int, int) {
+	val := &big.Int{}
+	val.SetBytes([]byte(ip))
+	if len(ip) == net.IPv4len {
+		return val, 32
+	} else if len(ip) == net.IPv6len {
+		return val, 128
+	} else {
+		panic(fmt.Errorf("Unsupported address length %d", len(ip)))
+	}
+}
+
+func intToIP(ipInt *big.Int, bits int) net.IP {
+	ipBytes := ipInt.Bytes()
+	ret := make([]byte, bits/8)
+	// Pack our IP bytes into the end of the return array,
+	// since big.Int.Bytes() removes front zero padding.
+	for i := 1; i <= len(ipBytes); i++ {
+		ret[len(ret)-i] = ipBytes[len(ipBytes)-i]
+	}
+	return net.IP(ret)
+}
+
+// addressRange returns the first and last addresses in the given CIDR range.
+func AddressRange(network *net.IPNet) (net.IP, net.IP) {
+	// the first IP is easy
+	firstIP := network.IP
+
+	// the last IP is the network address OR NOT the mask address
+	prefixLen, bits := network.Mask.Size()
+	if prefixLen == bits {
+		// Easy!
+		// But make sure that our two slices are distinct, since they
+		// would be in all other cases.
+		lastIP := make([]byte, len(firstIP))
+		copy(lastIP, firstIP)
+		return firstIP, lastIP
+	}
+
+	firstIPInt, bits := ipToInt(firstIP)
+	hostLen := uint(bits) - uint(prefixLen)
+	lastIPInt := big.NewInt(1)
+	lastIPInt.Lsh(lastIPInt, hostLen)
+	lastIPInt.Add(lastIPInt, firstIPInt)
+
+	return firstIP, intToIP(lastIPInt, bits)
+}
---
+++ internal/utils/ip_test.go
@@ -0,0 +1,48 @@
+package utils_test
+
+import (
+	"net"
+	"testing"
+
+	"github.com/stv0g/gont/internal/utils"
+)
+
+func TestIPAddressRangeV4(t *testing.T) {
+	_, src, err := net.ParseCIDR("192.168.0.0/24")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fromAddr, toAddr := utils.AddressRange(src)
+
+	expFromAddr := net.ParseIP("192.168.0.0")
+	expToAddr := net.ParseIP("192.168.1.0")
+
+	if !fromAddr.Equal(expFromAddr) {
+		t.Fail()
+	}
+
+	if !toAddr.Equal(expToAddr) {
+		t.Fail()
+	}
+}
+
+func TestIPAddressRangeV6(t *testing.T) {
+	_, src, err := net.ParseCIDR("fc00::/8")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fromAddr, toAddr := utils.AddressRange(src)
+
+	expFromAddr := net.ParseIP("fc00::")
+	expToAddr := net.ParseIP("fd00::")
+
+	if !fromAddr.Equal(expFromAddr) {
+		t.Fail()
+	}
+
+	if !toAddr.Equal(expToAddr) {
+		t.Fail()
+	}
+}
