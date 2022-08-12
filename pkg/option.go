06d476c237391d6f03ba828c24a8090c05b61b50
Steffen Vogel <post@steffenvogel.de>
2022-08-12 13:44:31 +0200

    add option customization helper
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- pkg/option.go
+++ pkg/option.go
@@ -1,3 +1,12 @@
 package gont
 
 type Option any
+
+func Customize(opts []Option, extraOptions ...Option) []Option {
+	new := []Option{}
+
+	new = append(new, opts...)
+	new = append(new, extraOptions...)
+
+	return new
+}
