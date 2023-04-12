532fd988be80669e4ffd64363bcfe3056b90be71
Steffen Vogel <post@steffenvogel.de>
2023-04-12 08:38:27 +0200

    trace: add level constants
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- pkg/trace/event.go
+++ pkg/trace/event.go
@@ -32,6 +32,25 @@ func init() {
 	}.EncMode()
 }
 
+const (
+	DebugLevel uint8 = iota + 1
+	// InfoLevel is the default logging priority.
+	InfoLevel
+	// WarnLevel logs are more important than Info, but don't need individual
+	// human review.
+	WarnLevel
+	// ErrorLevel logs are high-priority. If an application is running smoothly,
+	// it shouldn't generate any error-level logs.
+	ErrorLevel
+	// DPanicLevel logs are particularly important errors. In development the
+	// logger panics after writing the message.
+	DPanicLevel
+	// PanicLevel logs a message, then panics.
+	PanicLevel
+	// FatalLevel logs a message, then calls os.Exit(1).
+	FatalLevel
+)
+
 type EventCallback func(e Event)
 
 type Event struct {
