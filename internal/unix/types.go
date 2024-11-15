8468181fe318b1dfaa4aefa0de48ae16636bfbe5
Steffen Vogel <post@steffenvogel.de>
2024-11-15 20:49:14 +0100

    feat: Use PIDFDs to start sub-processes
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

---
+++ internal/unix/types.go
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2024 Steffen Vogel <post@steffenvogel.de>
+// SPDX-License-Identifier: Apache-2.0
+
+package unix
+
+type SiginfoChld struct {
+	Signo  int32     // Signal number.
+	Errno  int32     // If non-zero, an errno value associated with this signal.
+	Code   int32     // Signal code.
+	_      int32     // Padding
+	Pid    int       // Which child.
+	Status int       // Exit value or signal.
+	Uid    int       // Real user ID of sending process.
+	_      [100]byte // Padding
+}
