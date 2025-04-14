482b703ac79f5e0b42a02d352772fea1fed52b6c
Steffen Vogel <post@steffenvogel.de>
2025-04-14 18:46:22 +0200

    feat(options): Add new option to disable IPv6 networking
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- pkg/options/network.go
+++ pkg/options/network.go
@@ -13,3 +13,10 @@ type Persistent bool
 func (p Persistent) ApplyNetwork(n *g.Network) {
 	n.Persistent = bool(p)
 }
+
+// Disable IPv6.
+type IPv6Disabled bool
+
+func (d IPv6Disabled) ApplyNetwork(n *g.Network) {
+	n.IPv6Disabled = bool(d)
+}
